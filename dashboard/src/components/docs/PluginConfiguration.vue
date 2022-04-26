7942d60441b84115519ae716bf02a5d37c435b0c
cvalentin7 <77013631+cvalentin7@users.noreply.github.com>
2022-04-26 18:21:34 +0300

    Update PluginConfiguration.vue

--- dashboard/src/components/docs/PluginConfiguration.vue
+++ dashboard/src/components/docs/PluginConfiguration.vue
@@ -1,133 +1,129 @@
 <template>
   <div>
-    <h1>Plugin configuration</h1>
+    <h1>Configurare plugin</h1>
     <p>
-      Most plugins in Zeppelin have configurable options. The values for these options come from 3 places:
+      Majoritatea pluginurilor din Aura au opțiuni configurabile. Valorile pentru aceste opțiuni provin din 3 locuri:
     </p>
     <ol>
       <li>
-        <strong>Default options</strong> (from Zeppelin)
+        <strong>Opțiuni implicite</strong>  (de la Aura)
       </li>
       <li>
-        <strong>Custom options</strong> (set by you in config)
+        <strong>Opțiuni personalizate</strong> (setat de tine în configurare)
       </li>
       <li>
-        <strong>Overrides</strong> (conditional config values, see below)
+        <strong>Suprascrie</strong> (valori de configurare condiționate, vezi mai jos)
       </li>
     </ol>
     <p>
-      Permissions are also just regular config options with a <code>true</code>/<code>false</code> value.
-      <router-link to="/docs/configuration/permissions">See the Permissions page for more info.</router-link>
+      Permisiunile sunt, de asemenea, doar opțiuni de configurare obișnuite cu o valoare <code>true</code>/<code>false</code> .
+      <router-link to="/docs/configuration/permissions">Consultați pagina Permisiuni pentru mai multe informații.</router-link>
     </p>
     <p>
-      Information about each plugin's options can be found on the plugin's page, which can be accessed from the sidebar.
-      <router-link to="/docs/configuration/configuration-format">See the Configuration format page for an example of a full config.</router-link>
+      Informații despre opțiunile fiecărui plugin pot fi găsite pe pagina pluginului, care poate fi accesată din bara laterală.
+      <router-link to="/docs/configuration/configuration-format">Consultați pagina Format de configurare pentru un exemplu de configurare completă.</router-link>
     </p>
 
-    <h2>Overrides</h2>
+    <h2>Suprascrie</h2>
     <p>
-      Overrides are the primary mechanism of changing options and permissions based on permission levels, roles,
-      channels, user ids, etc.
+      Suprascrierile sunt mecanismul principal de modificare a opțiunilor și a permisiunilor pe baza nivelurilor de permisiune, roluri, canale, ID-uri de utilizator etc.
     </p>
 
     <Expandable class="wide">
-      <template v-slot:title>Click to see examples of different types of overrides</template>
+      <template v-slot:title>Faceți clic pentru a vedea exemple de diferite tipuri de suprascrieri</template>
       <template v-slot:content>
         <CodeBlock code-lang="yaml">
           plugins:
             example_plugin:
               config:
                 can_kick: false
-                kick_message: "You have been kicked"
+                kick_message: "Ai fost dat cu afara"
                 nested:
                   value: "Hello"
                   other_value: "Foo"
               overrides:
-                # Simple permission level based override to allow kicking only for levels 50 and up
+                # Modificare simplă bazată pe nivel de permisiune pentru a permite lovitura numai pentru nivelurile 50 și mai mari
                 - level: '>=50'
                   config:
                     can_kick: true
                     nested:
-                      # This only affects nested.other_value; nested.value is still "Hello"
+                      # Acest lucru afectează numai nested.other_value; nested.value este încă „Hello”
                       other_value: "Bar"
-                # Channel override - don't allow kicking on the specified channel
-                - channel: "109672661671505920"
+                # Suprascriere canal - nu permiteți lovirea pe canalul specificat
+                - channel: "940059682897272832"
                   config:
                     can_kick: false
-                # Don't allow kicking on any trhead
+                # Nu permiteți lovirea în niciun fir
                 - is_thread: true
                   config:
                     can_kick: false
-                # Don't allow kicking on a specific thread
-                - thread_id: "109672661671505920"
+                # Nu permiteți lovirea într-un anumit thread
+                - thread_id: "967511008899891220"
                   config:
                     can_kick: false
-                # Same as above, but for a full category
-                - category: "360735466737369109"
+                # La fel ca mai sus, dar pentru o categorie completă
+                - category: "938909326888812635"
                   config:
                     can_kick: false
-                # Multiple channels. If any of them match, this override applies.
-                - channel: ["109672661671505920", "570714864285253677"]
+                # Canale multiple. Dacă oricare dintre ele se potrivește, se aplică această suprascriere.
+                - channel: ["940059682897272832", "939287960552108102"]
                   config:
                     can_kick: false
-                # Match based on a role
-                - role: "172950000412655616"
+                # Potrivire pe baza unui rol
+                - role: "952961935316508722"
                   config:
                     can_kick: false
-                # Match based on multiple roles. The user must have ALL roles mentioned here for this override to apply.
-                - role: ["172950000412655616", "172949857164722176"]
+                # Potrivire bazată pe mai multe roluri. Utilizatorul trebuie să aibă TOATE rolurile menționate aici pentru ca această modificare să se aplice.
+                - role: ["952961935316508722", "955526681181978654"]
                   config:
                     can_kick: false
-                # Match on user id
-                - user: "106391128718245888"
+                # Potrivire pe ID de utilizator
+                - user: "401063006034984960"
                   config:
-                    kick_message: "You have been kicked by Dragory"
-                # Match on multiple conditions
-                - channel: "109672661671505920"
-                  role: "172950000412655616"
+                    kick_message: "Ai fost dat cu piciorul de Valentin"
+                # Se potrivește în mai multe condiții
+                - channel: "940059682897272832"
+                  role: "952961935316508722"
                   config:
                     can_kick: false
-                # Match on ANY of multiple conditions
+                # Se potrivește mai multor condiții
                 - any:
-                  - channel: "109672661671505920"
-                  - role: "172950000412655616"
+                  - channel: "940059682897272832"
+                  - role: "952961935316508722"
                   config:
                     can_kick: false
-                # Match on either of two complex conditions
+                # Potriviți în oricare dintre cele două condiții complexe
                 - any:
                   - all:
-                    - channel: "109672661671505920"
-                      role: "172950000412655616"
+                    - channel: "940059682897272832"
+                      role: "952961935316508722"
                     - not:
-                        role: "473085927053590538"
-                  - channel: "534727637017559040"
-                    role: "473086848831455234"
+                        role: "952961935316508722"
+                  - channel: "940059682897272832"
+                    role: "952961935316508722"
                   config:
                     can_kick: false
         </CodeBlock>
       </template>
     </Expandable>
 
-    <h2>Default overrides</h2>
+    <h2>suprascrieri implicite</h2>
     <p>
-      Many plugins have some overrides by default, usually for the default mod level (50) and/or the default admin level
-      (100). These are applied before any custom overrides in the config.
+      Multe plugin-uri au unele suprascrieri în mod implicit, de obicei pentru nivelul implicit de mod (50) și/sau nivelul implicit de administrator (100). Acestea sunt aplicate înaintea oricăror modificări personalizate în configurație.
     </p>
     <p>
-      You can see the default overrides for each plugin by checking the <strong>Default configuration section</strong>
-      under the <strong>Configuration tab</strong> on the plugin's documentation page.
+      Puteți vedea suprascrierile implicite pentru fiecare plugin, verificând secțiunea<strong> Configurare implicită </strong>
+      din fila <strong>Configurare de pe pagina de documentație a pluginului.</strong> 
     </p>
     <p>
-      To replace a plugin's default overrides entirely, set <code>replaceDefaultOverrides</code> to <code>true</code> in
-      plugin options, on the same level as <code>config</code> and <code>overrides</code>. In the following example, any
-      default overrides the plugin had will no longer have an effect:
+      Pentru a înlocui complet suprascrierile implicite ale unui plugin, setați <code>replaceDefaultOverrides</code> la <code>true</code> în opțiunile de plugin, la același nivel cu <code>config</code> și <code>overrides</code>. În exemplul următor, orice suprascrieri implicite pe care pluginul le-a avut nu va mai avea efect:
     </p>
 
     <CodeBlock code-lang="yaml">
       example_plugin:
         config:
           can_kick: false
-        replaceDefaultOverrides: true # <-- Here
+        replaceDefaultOverrides: true # <-- Aici
         overrides:
           - level: ">=25"
             config:
