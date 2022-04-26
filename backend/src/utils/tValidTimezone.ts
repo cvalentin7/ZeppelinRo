259a6dd1c6b4ae2e9198c7a0d259133089e1aa58
cvalentin7 <77013631+cvalentin7@users.noreply.github.com>
2022-04-26 20:10:50 +0300

    Update tValidTimezone.ts

--- backend/src/utils/tValidTimezone.ts
+++ backend/src/utils/tValidTimezone.ts
@@ -7,7 +7,7 @@ export const tValidTimezone = new t.Type<string, string>(
   (s): s is string => typeof s === "string",
   (from, to) =>
     either.chain(t.string.validate(from, to), (input) => {
-      return isValidTimezone(input) ? t.success(input) : t.failure(from, to, `Invalid timezone: ${input}`);
+      return isValidTimezone(input) ? t.success(input) : t.failure(from, to, `Fus orar nevalid: ${input}`);
     }),
   (s) => s,
 );
